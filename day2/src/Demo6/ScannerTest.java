85c853e2eaeeeaf1893535c04de1674fc8424750
54929 <549297913@qq.com>
2020-05-04 15:33:30 +0800

    1

--- day2/src/Demo6/ScannerTest.java
+++ day2/src/Demo6/ScannerTest.java
@@ -6,6 +6,9 @@ public class ScannerTest {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("请输入第一个数据：");
-        
+        int a = s.nextInt();
+        System.out.println("请输入第二个数据：");
+        int b = s.nextInt();
+        System.out.println(a + b);
     }
 }
---
+++ day2/src/Demo6/ScannerTest2.java
@@ -0,0 +1,11 @@
+package Demo6;
+
+import java.util.Scanner;
+
+public class ScannerTest2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("请输入第一个数：");
+        
+    }
+}
