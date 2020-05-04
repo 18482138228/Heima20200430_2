1f779d048e2b15d2dd462c41db97ac0d02b347ac
54929 <549297913@qq.com>
2020-05-04 16:16:21 +0800

    1

--- day2/src/Demo6/ScannerTest2.java
+++ day2/src/Demo6/ScannerTest2.java
@@ -6,6 +6,15 @@ public class ScannerTest2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("请输入第一个数：");
-        
+        int a = s.nextInt();
+        System.out.println("请输入第二个数：");
+        int b = s.nextInt();
+        boolean flag = (a == b);
+        if(flag){
+            System.out.println("两数相等");
+        }
+        else{
+            System.out.println("两数不相等");
+        }
     }
 }
