948ed555f8cb6bcbd8606efa4b8978c0e610abac
18482138228 <549297913@qq.com>
2020-06-03 10:56:07 +0800

    1

---
+++ day3/src/Demo2/ForDemo.java
@@ -0,0 +1,22 @@
+package Demo2;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("-------------------------");
+
+        for(int x=1;x<=
+                10;x++){
+            System.out.println("HelloWorld");
+        }
+    }
+}
---
+++ day3/src/Demo2/SwitchDemo.java
@@ -0,0 +1,39 @@
+package Demo2;
+
+import java.util.Scanner;
+
+public class SwitchDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个数字：1--7");
+        int weekday = sc.nextInt();
+        switch(weekday){
+            case 1:
+                System.out.println("星期一");
+                break;
+            case 2:
+                System.out.println("星期二");
+                break;
+            case 3:
+                System.out.println("星期三");
+                break;
+            case 4:
+                System.out.println("星期四");
+                break;
+            case 5:
+                System.out.println("星期五");
+                break;
+            case 6:
+                System.out.println("星期六");
+                break;
+            case 7:
+                System.out.println("星期天");
+                break;
+            default:
+                System.out.println("输入的数字有误");
+                break;
+
+
+        }
+    }
+}
