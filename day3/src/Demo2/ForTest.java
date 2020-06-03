1c7f0a359d562186f0b4e9975cd3b71f2ff0249b
18482138228 <549297913@qq.com>
2020-06-03 13:59:25 +0800

    1

--- day3/src/Demo2/ForTest.java
+++ day3/src/Demo2/ForTest.java
@@ -1,5 +1,21 @@
 package Demo2;
 
 public class ForTest {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println("-----------");
 
+        for (int x=1;x<=10;x++){
+            System.out.println(x);
+        }
+        System.out.println("-----------");
+
+        for (int x=5;x>=1;x--){
+            System.out.println(x);
+        }
+    }
 }
---
+++ day3/src/Demo2/ForTest2.java
@@ -0,0 +1,11 @@
+package Demo2;
+
+public class ForTest2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int x=1;x<=5;x++){
+            sum +=x;
+        }
+        System.out.println(sum);
+    }
+}
