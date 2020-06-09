649e4f0dadbc7ed54a48d841b6cd718d8a581b48
54929 <549297913@qq.com>
2020-06-09 16:57:24 +0800

    1

--- day4/src/ArrayDemo.java
+++ day4/src/ArrayDemo.java
@@ -1,5 +1,7 @@
 public class ArrayDemo {
     public static void main(String[] args) {
-        int []arr = new int[3];
+        //int []arr = new int[3];
+        int []arr = {1,2,3};
+        System.out.println(arr[3]);
     }
 }
---
+++ day4/src/ArrayTest4.java
@@ -0,0 +1,20 @@
+public class ArrayTest4 {
+    public static void main(String[] args) {
+        int []arr = {11,22,33,44,55};
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        System.out.println("-----------");
+
+        for (int x=0;x<5;x++){
+            System.out.println(arr[x]);
+        }
+
+        System.out.println("----------------");
+
+
+        System.out.println("数组的长度为"+arr.length);
+    }
+}
